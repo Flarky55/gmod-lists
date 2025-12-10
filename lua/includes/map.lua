d9cca08afa283937f0ed340c4e26b83f50b53861
Flarky55 <60645124+Flarky55@users.noreply.github.com>
2025-12-10 22:12:37 +0600

    ListMap -> map

---
+++ lua/includes/map.lua
@@ -0,0 +1,67 @@
+local function map()
+    local instance, mt = newproxy(), {}
+    debug.setmetatable(instance, mt)
+
+
+    local list, keys, positions, len = {}, {}, {}, 0
+    local index = {}
+
+    function mt:__len()
+        return len
+    end
+
+    function mt:__index(k)
+        return index[k] or list[k]
+    end
+
+    function mt:__newindex(k, v)
+        if v == nil then
+            local value = list[k]
+            if value == nil then return end
+
+            list[k] = nil
+
+            local lastkey = keys[len]
+            local pos = positions[k]
+
+            keys[pos] = lastkey
+            keys[len] = nil
+
+            positions[lastkey] = pos
+            positions[k] = nil
+
+            len = len - 1
+
+            return
+        end
+
+        if list[k] == nil then
+            len = len + 1
+
+            keys[len] = k
+            positions[k] = len
+
+            list[k] = v
+        end
+
+        list[k] = v
+    end
+
+    index.ipairs = function()
+        local i = 0
+        local key
+
+        return function()
+            i = i + 1
+            key = keys[i]
+
+            if key == nil then return nil end
+
+            return i, list[key]
+        end
+    end
+
+    return instance
+end
+
+_G.map = map
